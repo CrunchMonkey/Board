8bd30b6e96275a81aa4e3575ad7cdfebb90fe2d2
이승렬 <dltmdfuf95@naver.com>
2023-01-22 20:49:57 +0900

    임시이미지 추가, 메인화면 타일로 나누는것 필요

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static("../images"));
+//app.use(express.static("../images"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"../views")); //현재폴더 상위폴더에 views
