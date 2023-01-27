04e45dd1af7fa543635e43b3e5e7a5b310a02d41
이승렬 <dltmdfuf95@naver.com>
2023-01-27 19:09:18 +0900

    메인 재구성필요

--- server/server.js
+++ server/server.js
@@ -9,13 +9,12 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname,"../views")); //현재폴더 상위폴더에 views
 
 app.get('/', function (req, res) {
-    conn.query("SELECT * FROM newTable", (err, res) => {
-        console.log(err);
-        console.log(res);
-        conn.end();
-    });
+    // conn.query("SELECT * FROM newTable", (err, res) => {
+    //     console.log(err);
+    //     console.log(res);
+    //     conn.end();
+    // });
     res.render("main");
-    //res.send('Hello World');
 })
 
 app.listen(3000);
